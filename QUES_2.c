4e9af38a7ca309904770dc128af7de6c159457c0
Dinesh13-ux <bujji4160@gmail.com>
2025-08-11 15:23:57 +0530

    CRC sender and receiver

--- QUES_2.c
+++ QUES_2.c
@@ -14,12 +14,11 @@ void xor_div(char* msg,const char* poly, int msg_len,int poly_len){
 void compute_crc_receiver(const char* msg, const char* poly){
     int msg_len = strlen(msg);
     int poly_len = strlen(poly);
-    int total_len = msg_len + poly_len - 1;
 
     char temp[156];
     strcpy(temp,msg);
     xor_div(temp,poly,msg_len,poly_len);
-    printf("Received message: %s\n",msg);
+    printf("\nTransmitted message: %s\n",msg);
     printf("Generator polynomial: %s\n",poly);
     printf("CRC remainder: %s\n",&temp[msg_len- (poly_len -1)]);
 
@@ -37,7 +36,7 @@ void compute_crc_receiver(const char* msg, const char* poly){
         }
 
         org_data[msg_len- (poly_len -1)] = '\0';
-        printf("Decrypted message: %s\n\n",org_data);
+        printf("No Error detected. Original message: %s\n\n",org_data);
     }
     else{
         printf("Error occured during transmission no of error bits = %d\n",error);
@@ -70,28 +69,39 @@ void compute_crc_sender(const char* msg, const char* poly){
     strcpy(data + msg_len,&temp[msg_len]);
     data[total_len] = '\0';
     printf("Transmitted message: %s\n\n",data);
+
+    compute_crc_receiver(data,poly);
 }
 
 int main(){
+    char msg[256];
+    printf("Enter a message bit stream: ");scanf("%s",msg);
+    // const char* msg = "1101011011";
+    const char* crc12 = "1100000001111";
+    const char* crc16 = "11000000000000101";
+    const char* crcCCIP = "10001000000100001";
+    int inp;
+    printf("Enter your choice: \n");
+    while(1){ 
+        printf("1.CRC-12\n2.CRC-16\n3.CRC-CCIP\n");
+        scanf("%d",&inp);
+        if (inp == 1){
+            printf("== CRC-12 Computation == \n");
+            compute_crc_sender(msg,crc12);
+        }
+        else if (inp == 2){
+            printf("== CRC-16 Computation == \n");
+            compute_crc_sender(msg,crc16);
+        }
+        else if (inp == 3){
+            printf("== CRC-CCIP Computation == \n");
+            compute_crc_sender(msg,crcCCIP);
+        }
+        else{
+            printf("Program exited\n");
+            break;
+        }
+        
+    }
 
-    //using static input
-    const char* msg = "1101011011";
-    const char* crc = "10011";
-    compute_crc_sender(msg,crc);
-
-    const char* msg2 = "11110000";
-    const char* crc2 = "1001";
-    compute_crc_sender(msg2,crc2);
-
-
-    const char* enmsg = "11010110111110";
-    const char* crc3 = "10011";
-    compute_crc_receiver(enmsg,crc3);
-    // //using dynamic input
-    // char ch[15], ch_crc[15];
-    // printf("Enter a message bit stream: ");
-    // scanf("%s",ch);
-    // printf("Enter a generator polynomial bit stream: ");
-    // scanf("%s",ch_crc);
-    // compute_crc_sender(ch,ch_crc);
 }
