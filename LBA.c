221dd36d1a9d9eaae15e8897ebb221b5fbeb318e
Dinesh13-ux <bujji4160@gmail.com>
2025-10-06 14:14:13 +0530

    LBA algorithm

--- LBA.c
+++ LBA.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
-#define BUCKET_SIZE 10
-#define LEAK_RATE 2    
-
 int main() {
+    int bucket_size;
+    int leak_rate;
     int bucket = 0;
     int packets_in = 0;
     int i;
-
+    printf("Enter Bucket Size and Leak rate\n");
+    scanf("%d%d",&bucket_size,&leak_rate);
     printf("Leaky Bucket Algorithm Simulation\n");
-    printf("Bucket size: %d, Leak rate: %d packets/time unit\n\n", BUCKET_SIZE, LEAK_RATE);
 
     int packets_arrival[5] = {5, 7, 3, 4, 2};
 
@@ -20,16 +18,16 @@ int main() {
     int bucket_before = bucket;
     int status = 0;  // 0 = OK, 1 = Overflow
 
-    if(bucket + packets_in <= BUCKET_SIZE) {
+    if(bucket + packets_in <= bucket_size) {
         bucket += packets_in;
     } else {
         status = 1;
-        bucket = BUCKET_SIZE; 
+        bucket = bucket_size; 
     }
 
     int bucket_after_arrival = bucket;
 
-    int packets_sent = (bucket < LEAK_RATE) ? bucket : LEAK_RATE;
+    int packets_sent = (bucket < leak_rate) ? bucket : leak_rate;
     bucket -= packets_sent;
 
     printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%s\n", 
